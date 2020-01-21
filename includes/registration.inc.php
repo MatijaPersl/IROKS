91ae3ddb21e33f367cc2a6afea9fb2af1b4de359
Marko <marko.pavicevic@student.um.si>
2020-01-21 18:34:19 +0100

    Merge remote-tracking branch 'origin/master' into test-branch1
    Update za test-branch
    Da vidim če bo vredi branch updatalo. Ni sprememb v kodi, če slučajno mergam

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -6,30 +6,30 @@ if (isset($_POST['signup-submit'])) {
   $password = $_POST['pwd'];
   $passwordRepeat = $_POST['pwd-repeat'];
   if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
-    header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
+    header("Location: ../registration.php?error=emptyfields&uid=".$username."&mail=".$email);
     exit();
   }
   else if (!preg_match("/^[a-zA-Z0-9]*$/", $username) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: ../signup.php?error=invaliduidmail");
+    header("Location: ../registration.php?error=invaliduidmail");
     exit();
   }
   else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-    header("Location: ../signup.php?error=invaliduid&mail=".$email);
+    header("Location: ../registration.php?error=invaliduid&mail=".$email);
     exit();
   }
   else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: ../signup.php?error=invalidmail&uid=".$username);
+    header("Location: ../registration.php?error=invalidmail&uid=".$username);
     exit();
   }
   else if ($password !== $passwordRepeat) {
-    header("Location: ../signup.php?error=passwordcheck&uid=".$username."&mail=".$email);
+    header("Location: ../registration.php?error=passwordcheck&uid=".$username."&mail=".$email);
     exit();
   }
   else {
-    $sql = "SELECT uidUsers FROM users WHERE uidUsers=?;";
+    $sql = "SELECT username FROM users WHERE username=?;";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-      header("Location: ../signup.php?error=sqlerror");
+      header("Location: ../registration.php?error=sqlerror");
       exit();
     }
     else {
@@ -39,21 +39,21 @@ if (isset($_POST['signup-submit'])) {
       $resultCount = mysqli_stmt_num_rows($stmt);
       mysqli_stmt_close($stmt);
       if ($resultCount > 0) {
-        header("Location: ../signup.php?error=usertaken&mail=".$email);
+        header("Location: ../registration.php?error=usertaken&mail=".$email);
         exit();
       }
       else {
-        $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?);";
+        $sql = "INSERT INTO users (username, email, pwd) VALUES (?, ?, ?);";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-          header("Location: ../signup.php?error=sqlerror");
+          header("Location: ../registration.php?error=sqlerror");
           exit();
         }
         else {
           $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
           mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
           mysqli_stmt_execute($stmt);
-          header("Location: ../signup.php?signup=success");
+          header("Location: ../registration.php?signup=success");
           exit();
         }
       }
@@ -63,6 +63,6 @@ if (isset($_POST['signup-submit'])) {
   mysqli_close($conn);
 }
 else {
-  header("Location: ../signup.php");
+  header("Location: ../registrations.php");
   exit();
 }
