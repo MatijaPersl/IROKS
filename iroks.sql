093f09b3bf3706470a09bc3fddfe95faacde19b3
David Leskovar <david.leskovar1@student.um.si>
2020-01-26 21:12:29 +0100

    Popravljena podatkovna baza

--- iroks.sql
+++ iroks.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 19, 2020 at 07:27 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.1.33
+-- Gostitelj: 127.0.0.1
+-- Čas nastanka: 26. jan 2020 ob 21.07
+-- Različica strežnika: 10.4.8-MariaDB
+-- Različica PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `iroks`
+-- Zbirka podatkov: `iroks`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktura tabele `izdelki`
+--
+
+CREATE TABLE `izdelki` (
+  `id` int(255) NOT NULL,
+  `ime` varchar(255) NOT NULL,
+  `serijska` int(255) NOT NULL,
+  `kategorija` varchar(255) NOT NULL,
+  `cena` int(255) NOT NULL,
+  `opis` longtext NOT NULL,
+  `slika` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `users`
 --
 
 CREATE TABLE `users` (
@@ -36,28 +52,40 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Odloži podatke za tabelo `users`
 --
 
 INSERT INTO `users` (`idUsers`, `username`, `email`, `pwd`) VALUES
 (1, 'test', 'test@test.com', '$2y$10$p8hy/txutF3jtgqxGfvTOuOc9IOO3N0bIrLM0bP7ppO9DFBgc22yi');
 
 --
--- Indexes for dumped tables
+-- Indeksi zavrženih tabel
 --
 
 --
--- Indexes for table `users`
+-- Indeksi tabele `izdelki`
+--
+ALTER TABLE `izdelki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksi tabele `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`idUsers`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT zavrženih tabel
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT tabele `izdelki`
+--
+ALTER TABLE `izdelki`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `users`
 --
 ALTER TABLE `users`
   MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
